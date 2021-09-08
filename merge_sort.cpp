2c671076d12f6b7684f883f01352b5d6d3643c1c
icarus612 <ellishogan@gmail.com>
2021-09-08 18:58:51 -0400

    update

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,24 +3,28 @@
 
 using namespace std;
 
+void print_arr(vector<int> arr) {
+		for (int i = 0; i < arr.size(); i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+}
+
 vector<int> merge(vector<int> &left, vector<int> &right) {
 	vector<int> result;
 	while (left.size() > 0 && right.size() > 0) {
-		int l_end = left[left.size()-1];
-		int r_end = right[right.size()-1];
-		cout << "left: " << l_end << " | right: " << r_end << endl;
-		if (l_end > r_end) {
-			result.push_back(r_end);
-			right.pop_back();
+		int l_start = left[0];
+		int r_start = right[0];
+		if (l_start > r_start) {
+			result.insert(result.end(), r_start);
+			right.erase(right.begin());
 		} else {
-			result.push_back(l_end);
-			left.pop_back();
+			result.insert(result.end(), l_start);
+			left.erase(left.begin());
 		}
 	}
-
 	result.insert(result.end(), left.begin(), left.end());
 	result.insert(result.end(), right.begin(), right.end());
-
 	return result;
 }
 
@@ -28,24 +32,14 @@ vector<int> merge_sort(vector<int> &arr) {
 	if (arr.size() == 1) return arr;
 
 	int midpoint = (int) arr.size() / 2;
-	vector<int> left_arr, right_arr, new_left, new_right;
-	vector<int>::const_iterator itr_first = arr.begin();
-	vector<int>::const_iterator itr_middle = arr.begin() + midpoint;
-	vector<int>::const_iterator itr_last = arr.end();
-	new_left.assign(itr_first, itr_middle);
-	new_right.assign(itr_middle, itr_last); 
-
+	vector<int> left_arr, right_arr, new_left, new_right, new_arr;
+	new_left.assign(arr.begin(), arr.begin() + midpoint);
+	new_right.assign(arr.begin() + midpoint, arr.end()); 
 	left_arr = merge_sort(new_left);
 	right_arr = merge_sort(new_right);
 	return merge(left_arr, right_arr);
 }
 
-void print_arr(vector<int> arr) {
-		for (int i = 0; i < arr.size(); i++) {
-			cout << arr[i] << " ";
-		}
-		cout << endl;
-}
 int main() {
 
 	vector<int> test_arr, final_arr;
